bc971939ffd500e9b79e2f4a3dab7f8e77d4286c
Syed Jawad <jawadhussaini1804@gmail.com>
2024-05-21 17:11:32 +0500

    loops

--- loop.js
+++ loop.js
@@ -3,6 +3,16 @@
 // }
 
 
-for(let student=1; student<=50; student++){
-   console.log(student)
-}
+// for(let student=1; student<=50; student++){
+//    console.log(student);
+// };
+
+// let student ={
+//    saman: 10,
+//    jawad: 59, 
+//    hussain: 45,
+//    malik: 90
+// };
+// for(let a in student){
+//    console.log("Marks of " + a + " is " + student[a])
+// }
